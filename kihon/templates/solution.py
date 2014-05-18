6ece64620bb0dc5d8c62d0418d438a53667e77d9
Jose Antonio Leiva Izquierdo <jleivaizq@tmt.info>
2014-05-19 01:43:27 +0200

    Templates folder

---
+++ kihon/templates/solution.py
@@ -0,0 +1,12 @@
+import sys
+
+if __name__ == '__main__':
+
+	cases = int(sys.stdin.readline())
+
+    for case in range(cases):
+
+        # ... solve
+
+        sol = ''
+        print("Case #%d: %s" % (case + 1, sol))
